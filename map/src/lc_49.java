a311075cb60a417d30cabde9e78f189745a32141
DHH <ding248655>
2025-10-23 02:18:32 +0800

    10.22,复习49

--- map/src/lc_49.java
+++ map/src/lc_49.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @Author: DHH
@@ -28,5 +25,35 @@ public class lc_49 {
             list.add(str);
         }
         return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        new lc_49().groupAnagrams2(new String[]{"eat","tea","tan","ate","nat","bat"});
+    }
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map=new HashMap<>();
+        for (String item : strs){
+            String str=convert(item);
+            if (map.containsKey(str)){
+                map.get(str).add(item);
+            }else {
+                List<String> list=new LinkedList<>();
+                list.add(item);
+                map.put(str,list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+    private String convert(String str){
+        int[] map=new int[26];
+        for (char item : str.toCharArray()){
+            map[item-'a']++;
+        }
+        StringBuilder builder=new StringBuilder();
+        for (int i=0;i<26;i++){
+            builder.append(i+'a');
+            builder.append(map[i]);
+        }
+        return builder.toString();
     }
 }
